9903a07d5b58877a501e71827d348594ec867672
Yoshifumi Okazaki <yokazaki1086@gmail.com>
2022-04-08 09:52:32 +0900

    Update 1-5 Refactoring phase

--- app/Models/VacancyLevel.php
+++ app/Models/VacancyLevel.php
@@ -10,15 +10,23 @@ class VacancyLevel
         $this->remainingCount = $remainingCount;
     }
 
+    // mark メソッドの中身を変更
     public function mark(): string
     {
-        if ($this->remainingCount === 0) {
-            return '×';
-        }
-        if ($this->remainingCount < 5) {
-            return '△';
-        }
-        return '◎';
+        // if ($this->remainingCount === 0) {
+        //     return '×';
+        // }
+        // if ($this->remainingCount < 5) {
+        //     return '△';
+        // }
+        // return '◎';
+        
+        $marks = ['empty' => '×', 'few' => '△', 'enough' => '◎'];
+        $slug = $this->slug();
+        assert(isset($marks[$slug]), new \DomainException('invalid slug value.'));
+
+        return $marks[$slug];
+    
     }
 
     public function slug(): string
